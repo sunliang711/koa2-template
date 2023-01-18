bad72942af8599a86f28c13dcdfc17f46a8e467d
sunliang711 <sunliang711@163.com>
2023-01-18 17:14:58 +0800

    prefix of Router

--- src/routers/index.ts
+++ src/routers/index.ts
@@ -1,13 +1,14 @@
 import Router from 'koa-router'
 import health from './health'
 
-// const router = new Router({ prefix: '/api/v1' })
-const router = new Router()
+// full  path: [optional prefix] + 1st parameter of router.use + 1st parameter of router.get|post
+const router = new Router({ prefix: '/api/v1' })
 
-router.use('/api/v1', health.routes())
-// router.use('/api/v1', TODO.routes())
+router.use(health.routes())
+// router.use(TODO.routes())
+
 
 // debug, print all routes
-// console.log("router:", router)
+console.log("router:", router)
 
 export default router
