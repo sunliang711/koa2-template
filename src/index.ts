3564b53be179273e545556f4562d98fe2910421b
sunliang711 <sunliang711@163.com>
2023-01-18 17:33:57 +0800

    koa static

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,7 @@
 import Koa from 'koa'
 import responseTime from 'koa-response-time'
+import path from 'path'
+import serve from 'koa-static'
 
 import logger from 'koa-logger'
 import bodyParser from 'koa-bodyparser'
@@ -8,8 +10,11 @@ import config from './config'
 
 import router from './routers'
 
+const staticPath = './static'
 
 const app = new Koa()
+
+app.use(serve(path.join(__dirname, staticPath)))
 
 app.use(responseTime())
 app.use(logger())
